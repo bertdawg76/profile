e7ad4eed4a642c8d8588a99267138490f8fd03c2
bertdawg76 <bertcoleman76@gmail.com>
2016-06-14 01:28:01 -0600

    stuff

--- server/models/user.js
+++ server/models/user.js
@@ -58,7 +58,7 @@ UserSchema.methods.generateJwt = function(){
     role: this.role,
     name: this.name,
     exp: parseInt(expiry.getTime() / 1000)
-  }, secret.sessionSecret);
+  },  sessionSecret || secret.sessionSecret);
 };
 
 module.exports = mongoose.model('User', UserSchema);
